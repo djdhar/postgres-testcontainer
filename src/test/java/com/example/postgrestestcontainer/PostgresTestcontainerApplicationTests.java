3bd3b60224af061689d65d1fb14c4b409f191d54
Dibyajyoti Dhar 🤖 <dibyajyotidhar@gmail.com>
2024-05-20 19:03:05 +0530

    setup springboot with postgres server connection

---
+++ src/test/java/com/example/postgrestestcontainer/PostgresTestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgrestestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresTestcontainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
